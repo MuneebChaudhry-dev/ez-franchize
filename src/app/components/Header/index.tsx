6440216f07d2fe38f1aa723d8d2d059ff22f1fcf
Muneeb <muneebchaudhry.dev@gmail.com>
2024-03-06 17:18:40 +0500

    Add the Header Carousel Images

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import styles from "./header.module.css";
+import React from 'react';
+import styles from './header.module.css';
 import Navbar from '../Navbar';
+import Button from '../common/Button';
+import HeaderCarousel from '../HeaderCarousel';
 
 const Header = () => {
   return (
     <section className={styles.header}>
-      <Navbar/>
-    <div className=''>
-    </div>
+      <Navbar />
+      <div className={`${styles.headerContainer}`}>
+        <div className='container'>
+          <div className={styles.headerRow}>
+            <div className={styles.headerContent}>
+              <h1>Find the Best Franchises in Pakistan</h1>
+              <p>Get all relevent information you need in one place!</p>
+              <Button className={styles.availableFranchisesBtn}>
+                See Available Franchises
+              </Button>
+            </div>
+            <div className={styles.carousel}>
+              <HeaderCarousel />
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
